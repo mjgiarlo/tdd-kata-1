8858ba9010685b8ce4757aa5fd9f6549dda2c29b
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-10-07 16:42:06 -0700

    Improve delimiter finder regex and document it better

--- lib/string_calculator/delimiter_finder.rb
+++ lib/string_calculator/delimiter_finder.rb
@@ -1,29 +1,31 @@
 module StringCalculator
   class DelimiterFinder
+    def self.find(string)
+      new(string).find
+    end
+
     attr_reader :string
 
     def initialize(string)
       @string = string
     end
 
-    def call
+    def find
       default_delimiters + extract_delimiters
     end
 
     private
 
       def default_delimiters
-        ["\n", ","]
+        ["\n", ',']
       end
 
       def extract_delimiters
-        return [] unless string.match(%r{//.+\n})
-        # Yeah, this part sucks because I lacked the regex fu to combine these
-        if string.count('[') == 0
-          string.match(%r{//\[?(.+?)\]?\n}).captures
-        else
-          string.scan(%r{\[(.+?)\]}).flatten
-        end
+        string.scan(%r{
+          (?<=\[).+?(?=\])+ # Match one or more multi-character delimiters in brackets
+          |                 # ... or ...
+          (?<=//)[^\[]?     # Match a single non-bracket character after two forward slashes
+        }x)
       end
   end
 end
