db73293f1f7ea6da8a1b6ffd3a282c07fc9d0d97
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-10-07 16:43:13 -0700

    Move policy constants into module and use validator class

--- lib/string_calculator/base.rb
+++ lib/string_calculator/base.rb
@@ -1,11 +1,16 @@
-require 'string_calculator/tokenizer'
 require 'string_calculator/adder'
+require 'string_calculator/tokenizer'
+require 'string_calculator/validator'
 
 module StringCalculator
+  MINIMUM = 0
+  MAXIMUM = 1000
+
   class Base
     def add(string)
-      addends = Tokenizer.new(string).call
-      Adder.new(addends).call
+      Tokenizer.tokenize(string)
+        .then { |tokens| Validator.validate(tokens) }
+        .then { |addends| Adder.add(addends) }
     end
   end
 end
